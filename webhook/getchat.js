a284f385354488706e43f85f7bd48c0bb0b985e1
nyanko4 <nyankobetsuaka@outlook.com>
2026-01-31 17:25:20 +0900

    Implement getchat function for webhook handling

---
+++ webhook/getchat.js
@@ -0,0 +1,33 @@
+const CHATWORK_API_TOKEN = process.env.CWapitoken;
+const axios = require("axios");
+const reqcheck = require("../middleware/sign");
+const omikuji = require("../module/omikuji");
+
+async function getchat(req, res) {
+  const c = await reqcheck(req);
+  if (c !== "ok") {
+    return res.sendStatus(400);
+  }
+  console.log(req.body);
+  const {
+    body,
+    account_id: accountId,
+    room_id: roomId,
+    message_id: messageId,
+  } = req.body.webhook_event;
+  if (accountId == process.env.accountId) {
+    return res.sendStatus(200);
+  }
+  
+  const handlers = [omikuji];
+
+  for (const handler of handlers) {
+    if ((await handler(body, messageId, roomId, accountId)) === "ok") {
+      return res.sendStatus(200);
+    }
+  }
+
+  res.sendStatus(200);
+}
+
+module.exports = getchat;
