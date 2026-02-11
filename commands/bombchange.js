b74796efdb2045b6004c8be338e5ad6782f497ee
nyanko4 <nyankobetsuaka@outlook.com>
2026-02-11 15:20:56 +0900

    Create bombchange.js

---
+++ commands/bombchange.js
@@ -0,0 +1,8 @@
+const { status } = require("../module/bomb");
+const { sendchatwork } = require("../ctr/message");
+
+async function bombchange(body, messageId, roomId, messageId) {
+  return status.gakusei = true;
+}
+
+module.exports = bobmchange;
