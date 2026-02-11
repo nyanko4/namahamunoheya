beced8ff8d8860a714c7842dbebf21d4875bf3b5
nyanko4 <nyankobetsuaka@outlook.com>
2026-02-11 16:39:51 +0900

    Update bombchange.js

--- commands/bombchange.js
+++ commands/bombchange.js
@@ -3,7 +3,7 @@ const { sendchatwork } = require("../ctr/message");
 const isUserAdmin = require("../ctr/cwdata");
 
 async function bombchange(body, messageId, roomId, accountId) {
-  const isAdmin = isUserAdmin(accountId, roomId);
+  const isAdmin = await isUserAdmin(accountId, roomId);
   if (!isAdmin) return await sendchatwork(`[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]\n管理者のみ実行可能です`, roomId);
   status.gakusei = !status.gakusei;
   const newStatus = status.gakusei ? "ON" : "OFF";
