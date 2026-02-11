caf3de6fbfb9934f5359f8e39ee9568daec77458
nyanko4 <nyankobetsuaka@outlook.com>
2026-02-11 15:48:30 +0900

    Update bombchange.js

--- commands/bombchange.js
+++ commands/bombchange.js
@@ -1,8 +1,11 @@
 const { status } = require("../module/bomb");
 const { sendchatwork } = require("../ctr/message");
 
-async function bombchange(body, messageId, roomId, messageId) {
-  return status.gakusei = true;
+async function bombchange(body, messageId, roomId, accountId) {
+  status.gakusei = !status.gakusei;
+  const newStatus = status.gakusei ? "ON" : "OFF";
+  const message = `${newStatus} になりました`;
+  await sendchatwork(message, roomId);
 }
 
 module.exports = bombchange;
