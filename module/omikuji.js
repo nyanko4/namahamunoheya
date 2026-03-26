6d0da758b77fcfe8aec35c838d67d23cc23e9e31
nyanko4 <nyankobetsuaka@outlook.com>
2026-03-26 23:40:20 +0900

    Update omikuji.js

--- module/omikuji.js
+++ module/omikuji.js
@@ -3,14 +3,14 @@ const { sendchatwork } = require("../ctr/message");
 
 function getOmikujiResult() {
   const outcomes = [
-    { rate: 5.0, result: "大凶" },
-    { rate: 20.0, result: "小吉" },
-    { rate: 17.0, result: "末吉" },
-    { rate: 20.0, result: "吉" },
-    { rate: 15.0, result: "中吉" },
-    { rate: 10.0, result: "凶" },
-    { rate: 0.5, result: "熱湯がなんでもする券（猫化以外）" },
-    { rate: 12.5, result: "大吉" }
+      { rate: 5.0, result: "大凶" },
+      { rate: 20.0, result: "小吉" },
+      { rate: 20.0, result: "末吉" },
+      { rate: 20.0, result: "吉" },
+      { rate: 20.0, result: "中吉" },
+      { rate: 10.0, result: "凶" },
+      { rate: 0.1, result: "熱湯がなんでもする券(猫化系統以外)" },
+      { rate: 4.9, result: "大吉" }
   ];
   let random = Math.random() * 100;
   for (const { rate, result } of outcomes) {
