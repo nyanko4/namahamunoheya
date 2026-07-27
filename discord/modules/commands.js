9c2a9cc4d454c60468ae1f05cd8b4e5b54576363
nyanko4 <nyankobetsuaka@outlook.com>
2026-07-27 22:58:05 +0900

    Update commands.js

--- discord/modules/commands.js
+++ discord/modules/commands.js
@@ -1,4 +1,5 @@
 export { omikuji } from "../../commands/omikuji.js";
 export { debug } from "../commands/debug.js";
 export { executeCommand } from "../executeCommand.js";
+export { changeNickName } from "../commands/changeNickname.js";
 export { requestBotFunction } from "../commands/requestBotFunction.js";
