469f40589a45c12635f67a54853cc2e96a36d6ff
nyanko4 <nyankobetsuaka@outlook.com>
2026-07-27 23:21:44 +0900

    Update changeNickname.js

--- discord/commands/changeNickname.js
+++ discord/commands/changeNickname.js
@@ -1,11 +1,11 @@
-   
 export async function changeNickname(interaction) {
-  const target = interaction.options.getMember("target");
-  const nickname = interaction.options.getString("change-nickname");
+   const target = interaction.options.getMember("target");
+   const nickname = interaction.options.getString("change-nickname");
 
-  if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) {
-    return "ニックネームを変更する権限がありません";
-  }
+   if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) {
+      return "ニックネームを変更する権限がありません";
+   }
 
-  target.setNickname(nickname);
+   target.setNickname(nickname);
+   return `ユーザー${target}のニックネームを${nickname}に変更しました`;
 }
