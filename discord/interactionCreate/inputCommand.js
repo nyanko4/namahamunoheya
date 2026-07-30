40230a18d9d458dbd0cc167d1b7dfa70a118313e
nyanko4 <nyankobetsuaka@outlook.com>
2026-07-30 21:53:46 +0900

    Update inputCommand.js

--- discord/interactionCreate/inputCommand.js
+++ discord/interactionCreate/inputCommand.js
@@ -1,7 +1,7 @@
 // slashCommandを制御する関数
 
 import { MessageFlags } from "discord.js";
-import { omikuji, changeNickname, requestBotFunction } from "../modules/commands.js";
+import { omikuji, changeNickname, requestBotFunction, createEmoji, createStamp } from "../modules/commands.js";
 
 export async function handleInputCommand(interaction) {
   let result = "";
@@ -20,6 +20,14 @@ export async function handleInputCommand(interaction) {
   
   if (interaction.commandName == "要望") {
     result = await requestBotFunction(interaction);
+  }
+
+  if (interaction.commandName == "create-emoji") {
+    result = await createEmoji(interaction);
+  }
+
+  if (interaction.commandName == "create-stamp") {
+    result = await createStamp(interaction);
   }
   
   await interaction.editReply({ content: result });
