f82bcf9dc049389f1a77d4b51f9da3a0d608616a
nyanko4 <nyankobetsuaka@outlook.com>
2026-08-05 22:09:34 +0900

    Update createMessage.js

--- discord/commands/createMessage.js
+++ discord/commands/createMessage.js
@@ -5,8 +5,8 @@ import { client } from "../client.js";
 export async function createMessage(interaction) {
   const message = interaction.options.getString("message");
 
-  const channel = client.channels.fetch(LOG_ROOM_ID);
-  const target = client.users.fetch(LOG_PERSON_ID);
+  const channel = await client.channels.fetch(LOG_ROOM_ID);
+  const target = await client.users.fetch(LOG_PERSON_ID);
     
   const embed = new EmbedBuilder()
     .setColor(0x00ff00)
