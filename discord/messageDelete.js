f694420c8d0f989721459e03f4f8e055a67e094b
nyanko4 <nyankobetsuaka@outlook.com>
2026-08-05 21:23:34 +0900

    Update messageDelete.js

--- discord/messageDelete.js
+++ discord/messageDelete.js
@@ -2,5 +2,5 @@ import { logger } from "../discord/modules/utils.js";
 import { LOG_ROOM_ID } from "./config.js";
 
 export async function handleMessageDelete(message) {
-  if (message.channelId != LOG_ROOM_ID) return await logger(message, null, "delete");
+  if (message.channelId == LOG_ROOM_ID) return await logger(message, null, "delete");
 }
