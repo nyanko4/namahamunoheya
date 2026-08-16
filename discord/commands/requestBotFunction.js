db2310a340b9f8d80e1aff1efe896b8297a49b8f
nyanko4 <nyankobetsuaka@outlook.com>
2026-08-16 22:35:48 +0900

    Update requestBotFunction.js

--- discord/commands/requestBotFunction.js
+++ discord/commands/requestBotFunction.js
@@ -28,7 +28,7 @@ export async function requestBotFunction(interaction) {
     components: [button]
   });
   
-  return "要望を送信しました";
+  return { content: "要望を送信しました" };
 }
 
 async function requestCommand(interaction) {
