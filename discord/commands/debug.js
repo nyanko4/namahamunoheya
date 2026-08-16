bf50b0fc7f435921f1192b6484ce87fbe24f4425
nyanko4 <nyankobetsuaka@outlook.com>
2026-08-16 22:38:05 +0900

    Update debug.js

--- discord/commands/debug.js
+++ discord/commands/debug.js
@@ -4,5 +4,5 @@ export let debugFlag = false;
 export async function debug(message) {
   if (message.author.id != BOT_OWNER_ID) return;
   debugFlag = !debugFlag;
-  return { content: `デバッグモードを${debugFlag ? "ON" : "OFF"}にしました` };
+  return `デバッグモードを${debugFlag ? "ON" : "OFF"}にしました`;
 }
