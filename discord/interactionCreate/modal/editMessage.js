cb75ad72c65606ec2207cc5bbf2ba9947a6b14d0
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-03 21:23:45 +0900

    Update editMessage.js

--- discord/interactionCreate/modal/editMessage.js
+++ discord/interactionCreate/modal/editMessage.js
@@ -1,7 +1,14 @@
+import { MessageFlags } from "discord.js";
+
 export async function editMessage(interaction) {
+  await interaction.deferReply({
+    flags: MessageFlags.Ephemeral
+  });
+  
   const messageId = interaction.customId.split(":")[1];
   const message = await interaction.channle.messages.fetch(messageId);
   const editMessage = interaction.fields.getStringSelectValues()
   
   await message.edit(editMessage);
+  await interaction.editReply("編集が完了しました");
 }
