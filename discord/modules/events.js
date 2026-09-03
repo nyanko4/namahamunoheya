31a93706870dc27847cec99a8c1421f5fde7f73e
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-03 09:30:56 +0900

    Fix typo in export statement for contextMenuCommand

--- discord/modules/events.js
+++ discord/modules/events.js
@@ -4,3 +4,4 @@ export { handleMessageUpdate } from "../messageUpdate.js";
 export { handleMessageDelete } from "../messageDelete.js";
 export { handleInputCommand } from "../interactionCreate/inputCommand.js";
 export { handleButton } from "../interactionCreate/button.js";
+export { hadnleContextMenuCommand } from "../interactionCreate/contextMenuCommand.js";
