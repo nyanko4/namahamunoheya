0e9c70251f3e98b9f8fee56b219e112bac3b04cf
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-03 12:19:13 +0900

    Update contextMenuCommand.js

--- discord/interactionCreate/contextMenuCommand.js
+++ discord/interactionCreate/contextMenuCommand.js
@@ -1,6 +1,15 @@
+import { MessageFlags } from "discord.js";
+
 export async function handleContextMenuCommand(interaction) {
   let result = "";
+
+  await interaction.deferReply({
+    flags: MessageFlags.Ephemeral
+  });
+  
   if (commandName == "edit-message") {
     result = await editMessage(interaction);
   }
+
+  await interaction.editReply(result);
 }
