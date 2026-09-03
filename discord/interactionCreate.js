9e98356da0718ca0e754d3462e51330d2ca1142a
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-03 09:29:53 +0900

    Update interactionCreate.js

--- discord/interactionCreate.js
+++ discord/interactionCreate.js
@@ -1,6 +1,7 @@
-import { handleInputCommand, handleButton } from "./modules/events.js";
+import { handleInputCommand, handleButton, handleContextMenuCommand } from "./modules/events.js";
 
 export async function handleInteractionCreate(interaction) {
   if (interaction.isChatInputCommand()) return await handleInputCommand(interaction);
   if (interaction.isButton()) return await handleButton(interaction);
+  if (interaction.isContextMenuCommand()) return await handleContextMenuCommand(interaction);
 }
