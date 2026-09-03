1648e4e2c3f55037c8e85a23b80a72c8387219e3
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-03 21:58:14 +0900

    Update editMessage.js

--- discord/interactionCreate/modal/editMessage.js
+++ discord/interactionCreate/modal/editMessage.js
@@ -7,7 +7,7 @@ export async function editMessage(interaction) {
   
   const messageId = interaction.customId.split(":")[1];
   const message = await interaction.channel.messages.fetch(messageId);
-  const editMessage = interaction.fields.getStringSelectValues()
+  const editMessage = interaction.fields.getStringSelectValues("editText")
   
   await message.edit(editMessage);
   await interaction.editReply("編集が完了しました");
