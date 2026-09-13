7175aa5229bccd0cd063c117fceb0299e91dcb00
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-13 23:15:20 +0900

    Update messageLogger.js

--- utils/messageLogger.js
+++ utils/messageLogger.js
@@ -1,5 +1,5 @@
 const { sendername, getFileUrl } = require("../ctr/cwdata");
-const { sendChatwork, uploadFileToChatwork } = require("../ctr/message");
+const { sendchatwork, uploadFileToChatwork } = require("../ctr/message");
 const { checkFileSize, downloadFile, deleteFile } = require("./editFile"); 
 const { MAX_FILE_SIZE_BYTES, botOwnerId, logRoomId } = require("./constants");
 const fs = require("fs");
@@ -11,13 +11,13 @@ async function messageLogger(body, messageId, roomId, accountId, event, sendtime
     if (body.includes("[info][title][dtext:file_uploaded][/title]")) {
       const url = await getFileUrl(body, roomId);
       if (!url) {
-        return await sendChatwork(`${name}\n[qt][qtmeta aid=${accountId} time=${sendtime}]${body}[/qt]`, logRoomId, "log");
+        return await sendchatwork(`${name}\n[qt][qtmeta aid=${accountId} time=${sendtime}]${body}[/qt]`, logRoomId, "log");
       }
     
       const fileSize = await checkFileSize(url.fileurl);
     
       if (fileSize === null) {
-        return await sendChatwork(`${name}\n[qt][qtmeta aid=${accountId} time=${sendtime}]${body}[/qt]`, logRoomId);
+        return await sendchatwork(`${name}\n[qt][qtmeta aid=${accountId} time=${sendtime}]${body}[/qt]`, logRoomId);
       }
     
       const isOverLimit = fileSize > MAX_FILE_SIZE_BYTES;
@@ -30,18 +30,18 @@ async function messageLogger(body, messageId, roomId, accountId, event, sendtime
           return;
         }
     
-        await uploadFileToChatwork(localFilePath, name, logRoomId, "log");
+        await uploadFileTochatwork(localFilePath, name, logRoomId, "log");
     
       } finally {
         await deleteFile(localFilePath);
       }
     } else {
       const time = event === "message_updated" ? updatetime : sendtime;
-      await sendChatwork(`${name} ${accountId}\n[qt][qtmeta aid=${accountId} time=${time}]${body}[/qt]`, logRoomId);
+      await sendchatwork(`${name} ${accountId}\n[qt][qtmeta aid=${accountId} time=${time}]${body}[/qt]`, logRoomId);
     }
   } catch (error) {
     console.error("messageLoggerError:", error.response?.data || error.message);
-    await sendChatwork(`[To:${botOwnerId}]ログ処理でエラーが発生しました: ${error.response?.data || error.message}`, logRoomId);
+    await sendchatwork(`[To:${botOwnerId}]ログ処理でエラーが発生しました: ${error.response?.data || error.message}`, logRoomId);
   }
 }
 
