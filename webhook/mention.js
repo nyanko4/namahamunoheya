b6b9b655b63a8ace58c61a3dc3c7a1c1c93b0341
nyanko4 <nyankobetsuaka@outlook.com>
2026-09-13 22:19:38 +0900

    Update mention.js

--- webhook/mention.js
+++ webhook/mention.js
@@ -1,7 +1,5 @@
 const msedit = require("../ctr/message");
-const { blockMember } = require("../ctr/filter");
 const { isUserAdmin } = require("../ctr/cwdata");
-const arashi = require("../module/arashi");
 
 
 async function mentionWebhook(req, res) {
@@ -9,7 +7,9 @@ async function mentionWebhook(req, res) {
   const roomId = req.body.webhook_event.room_id;
   const messageId = req.body.webhook_event.message_id;
   const body = req.body.webhook_event.body;
+  
   await msedit.readmessage(roomId, messageId);
+  
   if (body.includes("削除")) {
     const isAdmin = await isUserAdmin(accountId, roomId);
     if (isAdmin){
@@ -18,11 +18,6 @@ async function mentionWebhook(req, res) {
   } else {
     msedit.sendchatwork("管理者のみ利用可能です", roomId)
   }}
-  if (body.match(/\[toall\]/g)) {
-    const isAdmin = await isUserAdmin(accountId, roomId);
-    if (!isAdmin) {
-      blockMember(roomId, accountId, "toall");
-    }
     return "ok";
   }
 }
